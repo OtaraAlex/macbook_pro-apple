3d66f83fbdb378c6597976c2a797b172f25a831a
OtaraAlex <mbechealex61@gmail.com>
2025-10-21 19:28:42 +0300

    Add navigation bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+import Navbar from "./components/Navbar";
+
 const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline text-indigo-700">
-      Hello, Apple Macbook Pro
-    </h1>
+    <main>
+      <Navbar />
+    </main>
   );
 };
 
