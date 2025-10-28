0b9aef684b34537cf3410941eb3d428b345c1967
OtaraAlex <mbechealex61@gmail.com>
2025-10-28 16:46:37 +0300

    Add footer with shopping information and update footer links to point to relevant Apple legal pages

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,53 @@
-import React from "react";
+import { footerLinks } from "../constants/index.js";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  const year = new Date().getFullYear();
+  return (
+    <footer>
+      <div className="info">
+        <p>
+          More ways to shop:{" "}
+          <a
+            href="https://www.apple.com/retail/"
+            target="_blank"
+            rel="noreferrer noopener"
+            className="text-primary hover:underline"
+          >
+            Find an Apple Store
+          </a>{" "}
+          or{" "}
+          <a
+            href="https://locate.apple.com/"
+            target="_blank"
+            rel="noreferrer noopener"
+            className="text-primary hover:underline"
+          >
+            other retailer
+          </a>{" "}
+          near you. Or call{" "}
+          <a href="tel:1-800-692-7753" className="text-primary hover:underline">
+            1-800-MY-APPLE
+          </a>{" "}
+          (1-800-692-7753).
+        </p>
+        <img src="/logo.svg" alt="Apple logo" />
+      </div>
+
+      <hr />
+
+      <div className="links">
+        <p>Copyright © {year} Apple Inc. All rights reserved.</p>
+
+        <ul>
+          {footerLinks.map(({ label, link }) => (
+            <li key={label}>
+              <a href={link}>{label}</a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
